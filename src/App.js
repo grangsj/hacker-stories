1db3909465bdecf9ac1c6bc8905aa6d736da48d6
Steve Granger <grangsj@gmail.com>
2022-03-24 18:19:29 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to shit yourself.
+          Edit <code>src/App.js</code> and save to learn the code!
         </p>
         <a
           className="App-link"
