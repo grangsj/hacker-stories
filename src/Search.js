be33c4794f28cd62a28b29f774be1cdf891190ec
Steve Granger <grangsj@gmail.com>
2022-03-29 08:19:42 -0400

    day 2 edits

--- src/Search.js
+++ src/Search.js
@@ -2,10 +2,15 @@ import * as React from 'react';
 
 function Search() {
 
+    const handleChange = (event) => {
+        console.log(event.target.value);
+    }
+
+
     return (
         <div>
             <label htmlFor='search'>Search: </label>
-            <input id='search' type='text' />
+            <input id='search' type='text' onChange={handleChange} />
         </div>
     )
 
